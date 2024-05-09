c81464ad8af1197b57aa99438f664196a12323e4
Abhishek <theabhishek.srm@gmail.com>
2024-05-09 21:26:07 +0530

    basics of scope

---
+++ basics.js
@@ -0,0 +1,29 @@
+// let 
+
+// let a = 10   // global variable and local variable
+
+
+
+// if(10>5){
+//  let b = 20 // local variable => block
+//   a = 50
+// }
+
+// console.log(a) 
+
+// console.log(a) // 10
+// console.log(b)
+
+
+
+let a = 100 
+
+if(10<5){
+  a = 50
+}
+else{
+    a = 70
+}
+
+
+console.log(a) // 50
